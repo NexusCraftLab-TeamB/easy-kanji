3f326a9455b426476490ef6d20824a2a29bd1b4c
YuusukeSakamoto <124509500+YuusukeSakamoto@users.noreply.github.com>
2024-08-28 15:16:39 +0900

    header追加

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
   <div id="app">
+    <AppHeader />
     <router-view />
   </div>
 </template>
 
 <script>
+  import AppHeader from './components/layout/Header.vue';
+
   export default {
     name: 'App',
     components: {
+      AppHeader
     }
-  }
+  };
 </script>
 
 <style>
+body {
+  margin: 0px;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
