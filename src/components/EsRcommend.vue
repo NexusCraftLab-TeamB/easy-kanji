eeb8487a8db951ed1283f119c0a079b17ce2716a
kent uchida <kento885088@icloud.com>
2024-09-01 22:07:00 +0900

    searchresult

--- src/components/EsRcommend.vue
+++ src/components/EsRcommend.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="search-conditions">
+        <h1>おすすめ</h1>
         <h2>検索条件</h2>
         <div class="conditions">
             <div><strong>場所：</strong><span class="condition-item">有楽町</span><span class="condition-item">銀座</span></div>
